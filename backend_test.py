34e6cad9520be3dd5bc09b7129ff0dbd95adf31a
Siddhivinayak Dubey <siddhivinayakdubey@gmail.com>
2026-01-20 15:25:25 +0530

    non emergent texts

--- backend_test.py
+++ backend_test.py
@@ -4,7 +4,7 @@ import json
 from datetime import datetime, timedelta
 
 class DistanceHugAPITester:
-    def __init__(self, base_url="https://distancehug.preview.emergentagent.com"):
+    def __init__(self, base_url="https://silispace.onrender.com"):
         self.base_url = base_url
         self.api_url = f"{base_url}/api"
         self.tests_run = 0
